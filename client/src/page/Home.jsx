8b464af1d299451b910b68f80f6873e6a02038bf
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-04-23 21:16:11 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://artify-teyv.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
