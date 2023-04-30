97413d9642bbf28d81f1a44d50a77fbc8626b018
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-04-30 09:18:37 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-bk1y1k5i01gfi6w3.us.auth0.com"
+    clientId="XSBMXR499PgbpNiyBeIdlkEcG0fk3fLb"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <App />
+  </Auth0Provider>
 )
