207141a7abed5e949a6afbd5d9c8c30b793129d8
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-05-19 20:10:55 +0530

    Add files via upload

---
+++ client/src/page/No.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const No = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+        <img className='mix-blend-multiply' src="https://media.tenor.com/UNaQFutfOF0AAAAC/banana-crying-cat.gif" />
+        <h1>404 Page Not Found</h1>
+    </div>
+  )
+};
+export default No;
