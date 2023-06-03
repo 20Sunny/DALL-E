81424ed4eba29cf0ff56be6dffa7f997abefe29b
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-06-03 11:15:42 +0530

    Update CreatePost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -42,7 +42,7 @@ const CreatePost = () => {
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (err) {
-        Swal.fire({err},"Not able to generate image","error");
+        Swal.fire('OpenAI API has been Expired',"Not able to generate image","error");
       } finally {
         setGeneratingImg(false);
       }
